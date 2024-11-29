bca26bf53f69a9c4299701c9091d53ba7fcdbfe5
Louis Bruneteau <bruneteau.louis@gmail.com>
2024-11-29 16:38:29 +0100

    new database

--- src/controllers/lists.controller.ts
+++ src/controllers/lists.controller.ts
@@ -3,7 +3,7 @@ import { FastifyRequest, FastifyReply} from 'fastify';
 import { getNextId } from '../services';
 
 async function listLists(this: any, request: FastifyRequest, reply: FastifyReply) {
-    const listIter = this.level.listdb.iterator();
+    const listIter = this.lists.iterator();
     const result: List[] = [];
     for await (const [_, list] of listIter) {
         result.push(JSON.parse(list.toString()));
@@ -13,8 +13,8 @@ async function listLists(this: any, request: FastifyRequest, reply: FastifyReply
 
 const addToLists = async function(this: any, request: FastifyRequest, reply: FastifyReply) {
     const list: List = request.body as List;
-    list.id = await getNextId(this.level.listdb);
-    await this.level.listdb.put(list.id, JSON.stringify(list));
+    list.id = await getNextId(this.lists);
+    await this.lists.put(list.id, JSON.stringify(list));
     Promise.resolve(list).then(list => reply.send(list));
 }
 
@@ -28,13 +28,13 @@ const updateList = async function(this: any, request: FastifyRequest<{Params: {i
         reply.send({ message: 'ID in body does not match ID in URL' });
         return;
     }
-    const oldList = await this.level.listdb.get(id);
+    const oldList = await this.lists.get(id);
     if (!oldList) {
         reply.code(404);
         reply.send({ message: 'List not found' });
         return;
     }
-    await this.level.listdb.put(id, JSON.stringify(list));
+    await this.lists.put(id, JSON.stringify(list));
     return Promise.resolve(list).then(list => reply.send(list));
 }
 
--- src/plugins/level.ts
+++ src/plugins/level.ts
@@ -1,5 +1,6 @@
 import fp from "fastify-plugin";
-import level, { FastifyLeveldbOptions } from "@fastify/leveldb"
+import level, { FastifyLeveldbOptions} from "@fastify/leveldb"
+import sublevel from "subleveldown"
 
 export default fp<FastifyLeveldbOptions>(async (fastify, opts) => {
     fastify.register(level,
@@ -8,4 +9,11 @@ export default fp<FastifyLeveldbOptions>(async (fastify, opts) => {
             name: "listdb"
         }
     );
+    fastify.after(() => {
+        const db = fastify.level.listdb;
+        const lists = sublevel(db, "lists", { valueEncoding: "json" });
+        const items = sublevel(db, "items", { valueEncoding: "json" });
+        fastify.decorate("lists", lists);
+        fastify.decorate("items", items);
+    });
 });
