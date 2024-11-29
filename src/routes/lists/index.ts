caf2bcd0803327aa653e00d6f6903d2f07e5030d
Marc Blanchet <m.underray@gmail.com>
2024-11-29 12:00:35 +0100

    Fix

--- src/routes/lists/index.ts
+++ src/routes/lists/index.ts
@@ -3,13 +3,14 @@ import {
   listLists,
   addToLists,
   deleteFromLists,
-, updateList} from "../../controllers/lists.controller";
+  updateList,
+} from "../../controllers/lists.controller";
 
 async function lists(fastify: FastifyInstance) {
   fastify.get("/", listLists);
   fastify.post("/", addToLists);
   fastify.delete("/:id/items/:id", deleteFromLists);
-    fastify.put('/:id', updateList);
+  fastify.put("/:id", updateList);
 }
 
 export default lists;
