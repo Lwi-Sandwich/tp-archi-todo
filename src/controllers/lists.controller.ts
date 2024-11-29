78aefd2d9027e9b9e4d0b7b672930dcdeef9997b
Louis Bruneteau <bruneteau.louis@gmail.com>
2024-11-29 11:45:25 +0100

    des choses

--- src/controllers/lists.controller.ts
+++ src/controllers/lists.controller.ts
@@ -16,4 +16,23 @@ const addToLists = async function(this: any, request: FastifyRequest, reply: Fas
     Promise.resolve(list).then(list => reply.send(list));
 }
 
-export { listLists, addToLists };
+const updateList = async function(this: any, request: FastifyRequest, reply: FastifyReply) {
+    const { idAny }: any = request.params;
+    const id = parseInt(idAny);
+    const list: List = request.body as List;
+    console.log(list);
+    if (list.id !== id) {
+        reply.code(400);
+        reply.send({ message: 'ID in body does not match ID in URL' });
+        return;
+    }
+    const oldList = await this.level.listdb.get(id);
+    if (!oldList) {
+        reply.code(404);
+        reply.send({ message: 'List not found' });
+        return;
+    }
+    await this.level.listdb.put(id, JSON.stringify(list));
+}
+
+export { listLists, addToLists, updateList };
--- src/routes/lists/index.ts
+++ src/routes/lists/index.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from "fastify";
-import { listLists , addToLists} from "../../controllers/lists.controller";
+import { listLists , addToLists, updateList} from "../../controllers/lists.controller";
 
 async function lists(fastify: FastifyInstance) {
     fastify.get('/', listLists);
     fastify.post('/', addToLists);
+    fastify.put('/:id', updateList);
 }
 
 export default lists;
